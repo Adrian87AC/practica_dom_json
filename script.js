47740671dbd07eb002d90480ecfa8724133c270f
Adrián.Álvarez <adrian.alvarez@eusa.es>
2025-10-23 10:09:34 +0200

    segundos cambios

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ document.getElementsByTagName("p")[0].style.color = "lightgreen";
 Usa getElementsByClassName para recorrerlos con un for y cambiar el color de fondo a
 gris.*/
 
+//Ejercicio 3: Sección con querySelector
+/*
+Usa querySelector para seleccionar el primer <div> con clase .caja y cambia su borde y
+fondo con style.
+*/
+
+//Ejercicio 4: querySelectorAll y forEach
+/*
+Crea tres botones con clase btn-dom.
+Recógelos con querySelectorAll y cambia el texto de todos ellos a “DOM listo ”.
+*/
+
+//Ejercicio 5: Comparación entre colecciones vivas y estáticas
+/*
+Crea una lista <ul id="lista"> con dos <li>.
+Muestra por consola la diferencia entre getElementsByTagName("li") (viva) y
+querySelectorAll("li") (estática) al agregar un nuevo elemento.
+*/
+
+//Ejercicio 6:
+
+//Ejercicio 7:
+//Ejercicio 8:
+//Ejercicio 9:
+//Ejercicio 10:
+//Ejercicio 11:
+//Ejercicio 12:
+//Ejercicio 6:
